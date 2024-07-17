955033c1bf4e42c473caa61ad34746ca2a0df815
JoaoAugusto48 <jpaiaobonifacio@gmail.com>
2024-07-17 00:31:50 -0300

    Cards e Tabelas sendo melhorados e criados

--- resources/views/allowance/index.blade.php
+++ resources/views/allowance/index.blade.php
@@ -1,5 +1,22 @@
 <x-layout title="Allowance">
-    @foreach ($allowances as $allowance)
-        <p class="m-0">{{ $allowance->title }} - {{ $allowance->value }} - {{  $allowance->kindTransaction }} - {{ $allowance->account->accountHolder->name }}</p>
-    @endforeach
+
+    <div class="row row-cols-4">
+        @foreach ($allowances as $allowance)
+            <div class="row m-1">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $allowance->title }}</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">{{ $allowance->account->accountHolder->name }}
+                        </h6>
+                        <p class="card-text">{{ $allowance->kindTransaction }} - {{ $allowance->value }}</p>
+                        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                            <button type="button" class="btn btn-info"><i class="bi bi-eye"></i></button>
+                            <button type="button" class="btn btn-warning"><i class="bi bi-pencil-square"></i></button>
+                            <button type="button" class="btn btn-danger"><i class="bi bi-trash"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 </x-layout>
--- resources/views/bank/index.blade.php
+++ resources/views/bank/index.blade.php
@@ -1,29 +1,36 @@
 <x-layout title="Bank">
 
-    <p class="h1">Your Bank's</p>
-    
-    <table class="table align-middle">
-        <caption>List of Banks</caption>
-        <tbody>
-            @foreach ($banks as $bank)
-                <tr>
-                    <th scope="row">{{ $bank->number }}</th>
-                    <td>{{ $bank->name }}</td>
-                    <td>{{ $bank->abbreviation }}</td>
-                    <td>
-                        <div class="hstack gap-2">
-                            <div class="ms-auto">
-                                <button type="button" class="btn btn-info btn-sm">About</button>
-                            </div>
-                            <div class="vr"></div>
-                            <div class="">
-                                <button type="button" class="btn btn-danger btn-sm">Remove</button>
-                            </div>
-                        </div>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="clearfix">
+        <p class="h1 float-start">Bank</p>
+        <button type="button" class="btn btn-info btn float-end">Create</button>
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="col-10">
+            <table class="table table-hover align-middle">
+                <caption>List of Banks</caption>
+                <tbody>
+                    @foreach ($banks as $bank)
+                        <tr>
+                            <th scope="row" class="text-center">{{ $bank->number }}</th>
+                            <td>{{ $bank->name }}</td>
+                            <td>{{ $bank->abbreviation }}</td>
+                            <td>
+                                <div class="hstack gap-2">
+                                    <div class="ms-auto">
+                                        <button type="button" class="btn btn-info btn-sm">About</button>
+                                    </div>
+                                    <div class="vr"></div>
+                                    <div class="">
+                                        <button type="button" class="btn btn-danger btn-sm">Remove</button>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 
 </x-layout>
